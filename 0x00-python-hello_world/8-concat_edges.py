79fb20b79dd3ceb1abc2910a9718dfcf99212658
Faithchep414 <115191278+Faithchep414@users.noreply.github.com>
2023-03-08 00:39:36 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming"
-str2 = " with Python"
-print(str1[0:27] + str2)
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
+print(str1 + str2)
+print("{}{}".format(str1, str2))
+print("{}\n{}".format(str1, str2))
+print("{}".format(str1)[-2:] + "{}".format(str2)[:3])
+print("Welcome to {}!".format(str1 + " " + str2))
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+print("object" + "-oriented" + " programming" + " with " + "Python")
